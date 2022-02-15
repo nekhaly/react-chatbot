82739b60ae17400239a2ef61749ebe7e71538b33
nekhaly <nekhaly@gmail.com>
2022-02-15 08:48:28 +0100

    Add redux-persist support
    To allow persisting messages in browser localStorage.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import storeConfig from "./store";
 import { App } from "./components/App";
 import "./style.css";
 
 const rootElement = document.getElementById("app");
+const { store, persistor } = storeConfig();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App test="Test" />
+    <PersistGate loading={null} persistor={persistor}>
+      <App test="Test" />
+    </PersistGate>
   </Provider>
   , rootElement
 );
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,9 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { messagesReducer } from './reducer';
+import {configureStore} from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {combineReducers} from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+
+import { messagesReducer } from "./reducer";
+
+const reducers = combineReducers(
+	{ messages: messagesReducer, }
+)
+
+const persistConfig = {
+	key: 'root',
+	storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-	reducer: {
-		messages: messagesReducer,
-	},
-})
-export default store;
+	reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export default () => {
+	return { store, persistor }
+};
